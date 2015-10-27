30c52c7e0d7964ed25b2389301a92ae9e79648bf
muzaffarsharapov <sharapov0140@gmail.com>
2015-10-26 20:29:49 -0700

    set up title screen and create func tap title screen in FlappyBird class

---
+++ FlappySwift/FlappyBird.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  FlappySwift
+//
+//  Created by ZAF on 10/26/15.
+//  Copyright © 2015 InteractiveCoconut. All rights reserved.
+//
+
+import Foundation
+
--- FlappySwift/GameViewController.swift
+++ FlappySwift/GameViewController.swift
@@ -8,19 +8,40 @@
 
 
 import UIKit
-
+import SpriteKit
+import ActionSwift3
 
 
 class GameViewController: UIViewController {
     
     
-    
+    var flappyBird:FlappyBird?
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       let stage = Stage(self.view as! SKView)
+       flappyBird = FlappyBird()
+       stage.addChild(flappyBird!)
+       flappyBird!.setUpTitleScreen()
         
     }
 
 
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Portrait
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
     
 }
