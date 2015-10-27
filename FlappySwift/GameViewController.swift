4dcc3f0edb6e19d3665b01a04792a91ef92ca47b
muzaffarsharapov <sharapov0140@gmail.com>
2015-10-26 20:24:55 -0700

    Initial Commit

---
+++ FlappySwift/GameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameViewController.swift
+//  FlappySwift
+//
+//  Created by Zaf on 10/26/2015.
+//  Copyright © 2015 Muzaffar Sharapov. All rights reserved.
+//
+
+
+import UIKit
+
+
+
+class GameViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+
+    
+}
