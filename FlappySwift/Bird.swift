2740157aafd6b2a093105918a2cacc4b92f2b556
muzaffarsharapov <sharapov0140@gmail.com>
2016-02-06 05:41:26 -0800

    fix all the bugs

--- FlappySwift/Bird.swift
+++ FlappySwift/Bird.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2015 InteractiveCoconut. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ActionSwift3
+
+public class Bird: MovieClip {
+    private static let BOOSTER:int = 12
+    private static let GRAVITY_COEFFICIENT:int = 1
+    private static let MAX_GRAVITY:int = 5
+    private static let ANGLE:int = 45
+    
+    private var currentVelocity:int = 0
+    private var gravityOn = false
+    public init() {
+        super.init(textureNames: ["bird-1","bird-2","bird-3"], fps: 5)
+        self.mouseEnabled = false
+        self.addEventListener(EventType.EnterFrame.rawValue, EventHandler(enterFrame,"enterFrame"))
+    }
+    public func reset() {
+        self.rotation = 0
+        self.width = Stage.stageWidth / 8
+        self.scaleY = self.scaleX
+        self.x = Stage.stageWidth * 0.2
+        self.y = Stage.stageHeight * 0.3
+    }
+    public func turnGravityOn() {
+        gravityOn = true
+    }
+    public func turnGravityOff() {
+        gravityOn = false
+    }
+    public func turboBoost() {
+        if (!gravityOn) {return}
+        currentVelocity = -Bird.BOOSTER
+    }
+    private func enterFrame(event:Event) {
+        if (!gravityOn) {return}
+        currentVelocity += Bird.GRAVITY_COEFFICIENT
+        if (currentVelocity > Bird.MAX_GRAVITY) {
+            currentVelocity = Bird.MAX_GRAVITY
+        }
+        self.y+=Number(currentVelocity)
+        if (currentVelocity < -5) {
+            self.rotation = -Number(Bird.ANGLE)
+        } else if (currentVelocity > 5) {
+            self.rotation = Number(Bird.ANGLE)
+        } else {
+            self.rotation = 0
+        }
+    }
+}
+
--- FlappySwift/Tube.swift
+++ FlappySwift/Tube.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2015 InteractiveCoconut. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ActionSwift3
+
+public class Tube: MovieClip {
+    private var tubeTop:MovieClip
+    private var tubeBottom:MovieClip
+    public var randomGapBeginning:int = 0
+    public var gapHeight:int = 0
+    init() {
+        tubeBottom = MovieClip(textureNames: ["tube1"], fps: 1)
+        tubeTop = MovieClip(textureNames: ["tube2"], fps: 1)
+        
+        super.init(textureNames: [], fps: 1)
+        
+        self.mouseEnabled = false
+        
+        tubeBottom.width = Stage.stageWidth * 0.2
+        tubeBottom.scaleY = tubeBottom.scaleX
+        self.addChild(tubeBottom)
+        
+        
+        tubeTop.width = Stage.stageWidth * 0.2
+        tubeTop.scaleY = tubeTop.scaleX
+        self.addChild(tubeTop)
+        
+        self.gapHeight = int(Stage.stageHeight * 0.27)
+        let possibleGapBeginning:int = int(Stage.stageHeight * 0.2)
+        let possibleGapEnd:int = int(Stage.stageHeight * 0.7)
+
+        self.randomGapBeginning = possibleGapBeginning + int(arc4random_uniform(UInt32(possibleGapEnd - possibleGapBeginning - self.gapHeight)))
+        tubeTop.y = CGFloat(randomGapBeginning - int(tubeTop.height))
+        tubeBottom.y = CGFloat(randomGapBeginning + int(gapHeight))
+        
+        self.x = Stage.stageWidth
+    }
+    override public var width:CGFloat {
+        get {
+            return tubeTop.width
+        }
+        set(newValue) {
+            print("DO NOT DO THIS!")
+        }
+    }
+}
